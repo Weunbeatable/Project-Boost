83aecaacd7c8a1f100e1878bdb56b05bd51232d6
josh Olat <josuah9@hotmail.com>
2021-02-16 14:01:41 -0500

    added more sound effects and worked on sequencing the sounds so they play at appropriate intervals

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -2,65 +2,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float MainThrust = 200f;
+    [SerializeField] AudioClip MainEngine;
+    [SerializeField] AudioClip LevelComplete;
+    [SerializeField] AudioClip EngineFailiure;
+    int SceneIndex = 0;
 
     Rigidbody rigidBody;
-    AudioSource RocketAudioScourse;
+    AudioSource RocketAudioSource;
     // Start is called before the first frame update
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        RocketAudioScourse = GetComponent<AudioSource>();
+        RocketAudioSource = GetComponent<AudioSource>();
      
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+            // somewhere stop sound on death. 
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        switch (collision.gameObject.tag)
+        if (state != State.Alive) { return; }
+             // kind of like a input file stream check from C++ 
+
+            switch (collision.gameObject.tag)
         {
             case "friendly":
-                //do nothing
-                print("OK");
+            
                 break;
 
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                LevelCompleteSequence();
+                // parameterise time  
+                // invoke means at some time the loadnextscene will be called but during that time another 
+                //function can still be called. 
                 break;
 
             default:
-                print("Dead");
+                DeathSequence();
+                // parameterise the time so there is a delay after you die. 
 
-            break;
+                break;
         }
     }
-    private void Thrust()
+
+   
+
+    private void LevelCompleteSequence()
     {
-        if (Input.GetKey(KeyCode.Space))// can thrust while rotating
-        {
-            rigidBody.AddRelativeForce(Vector3.up * MainThrust);
-            if (!RocketAudioScourse.isPlaying) // so we dont have the same audio source layering on top of itself.
-            {
-                RocketAudioScourse.Play();
-            }
-            else
-            {
-                RocketAudioScourse.Stop();
-            }
+        state = State.Transcending;
+        RocketAudioSource.Stop();
+        RocketAudioSource.PlayOneShot(LevelComplete);
+        Invoke("LoadNextScene", 1f);
+    }
 
+    private void DeathSequence()
+    {
+        state = State.Dying;
+        RocketAudioSource.Stop();
+        RocketAudioSource.PlayOneShot(EngineFailiure);
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+    private  void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); // todo allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0); // loading the first level 
+    }
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {// can thrust while rotating
+            ApplyThrust();
+        }
+        else
+        {
+            RocketAudioSource.Stop();
         }
     }
-    private void Rotate()
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * MainThrust);
+        if (!RocketAudioSource.isPlaying) // so we dont have the same audio source layering on top of itself.
+        {
+            RocketAudioSource.PlayOneShot(MainEngine); // this allows us to deal with multiple audio clips
+        }
+
+
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
 
